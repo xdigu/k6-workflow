6ef1a4d8806e2f4df18413bf51620cdeb4de3186
Rodrigo Jorge <xdigu@live.com>
2025-08-19 18:47:27 -0300

    [fix]: fixing stage times

--- k6/default.k6.js
+++ k6/default.k6.js
@@ -6,13 +6,13 @@ import URLS from './urls.js'
 export const options = {
     stages: [
         // Ramp-up from 1 to 30 VUs in 30s
-        { duration: "2s", target: 30 },
+        { duration: "30s", target: 30 },
 
         // Stay on 30 VUs for 60s
-        { duration: "5s", target: 30 },
+        { duration: "60s", target: 30 },
 
         // Ramp-down from 30 to 0 VUs in 10s
-        { duration: "2s", target: 0 }
+        { duration: "30s", target: 0 }
     ]
 };
 
